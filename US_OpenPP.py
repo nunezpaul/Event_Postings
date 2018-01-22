f2e7265d6a6b78bf7d68ec5272814c59ac71dd3b
Paul N <nunez.paul@utexas.edu>
2018-01-21 19:38:45 -0800

    Update US_OpenPP.py

--- US_OpenPP.py
+++ US_OpenPP.py
@@ -1,19 +1,19 @@
 from selenium.webdriver.common.keys import Keys
 import tools.facebookEvent as fbE
 
-description = ((Keys.TAB, Keys.ARROW_DOWN, 
-        "What's up Caltech Westies!",
+description = ((Keys.ARROW_DOWN, 
+        " What's up Caltech Westies!",
         Keys.ENTER, Keys.ENTER, 
-        "Instead of hosting our weekly, we're hosting a carpool to the US Open Pre-Party!",
+        " Instead of hosting our weekly, we're hosting a carpool to the US Open Pre-Party! ",
         "It's somewhat of a tradition as this will be the third year in a row. ",
         "The time we leave is somewhat flexible and subject to change. ",
-        "The dance is $10 and goes till an ungodly hour most likely so be prepared!",
+        "The dance is $10 and goes till an ungodly hour most likely so be prepared! ",
         Keys.ENTER,
-        "See you there!",
+        " See you there!",
         Keys.ENTER, Keys.ENTER,
-        "Link to the event:",
+        " Link to the event:",
         Keys.ENTER,
-        "https://www.facebook.com/events/1986737624880114/"))
+        " https://www.facebook.com/events/1986737624880114/"))
 
 #http://goo.gl/maps/YTSL1 winnet lounge https://goo.gl/maps/gJ4Yvh9Qc982 parking for winnett
         
